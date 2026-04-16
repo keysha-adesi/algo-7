b4708eb42f9324db778e9f6eff0ce53484ce3e9d
keysha-adesi <keyshaputrii590@gmail.com>
2026-04-16 10:22:08 +0700

    masukkan banyaknya elemen pada array maksimal 10

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -1,1 +1,21 @@
 #include <iostream>
+using namespace std;
+
+int element [10];
+int npanjang;
+int x;
+
+void input ()
+{
+    while (true)
+    {
+        cout << "masukkan banyaknya element pada array (maksimal 10): ";
+        cin >> npanjang;
+
+        if (npanjang <= 10)
+        {
+            break;
+        }
+        
+    }
+}
