d16b55a57f846b09812dc3087e6aa6044d440d34
keysha-adesi <keyshaputrii590@gmail.com>
2026-04-16 11:07:48 +0700

    menambahkan fitur pencarian binary search

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -57,5 +57,56 @@ void display ()
     cout << " Element Array Setelah Diurutkan (Asc)\n";
     cout << "===================================\n";
 
-    for (int)
+    for (int j = 0; j < npanjang; j++)
+    {
+        cout << element[j];
+        if (j < npanjang - 1)
+        {
+            cout << " -> ";
+        }
 }
+cout << endl;
+}
+
+void binarySearch()
+{
+    char ulang;
+    do
+    {
+
+    cout  << "\n======================\n";
+    cout << "    Pencarian Binary Search \n";
+    cout << "============================\n";
+
+    cout << "Masukkan element yang ingin dicari: ";
+    cin >> x;
+
+    int low = 0;
+    int high = npanjang -1;
+
+
+    do
+    {
+        int mind = (low + high) /2;
+
+        if (element [mid] == x)
+        {
+            cout << "\n[✓] Element" << x << " ditemukan pada indeks " << mid << "\n";
+            return;
+        }
+        if (x < element[mid] == x)
+        {
+          high = mid -1;
+        }
+        if (x > element[mid])
+        {
+            low = mid + 1;
+        }
+    } 
+    
+    }
+    
+}
+    
+
+
